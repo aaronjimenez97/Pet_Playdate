d9cf0ff1f296a0cff47fcbf2e7ec8865bb0c97de
kenjiavila <kenji.avila@csu.fullerton.edu>
2019-10-18 18:35:46 -0700

    fixed buttons/added logout page/ fixed calender page

--- project_backend/public/scripts/calender.js
+++ project_backend/public/scripts/calender.js
@@ -1,12 +1,18 @@
-// Activate Carousel
-$("#myCarousel").carousel();
 
-// Enable Carousel Indicators
-$(".item").click(function(){
-  $("#myCarousel").carousel(1);
-});
 
-// Enable Carousel Controls
-$(".left").click(function(){
-  $("#myCarousel").carousel("prev");
-});
+
+// $(document).ready(function() {
+//
+//   // Activate Carousel
+//   $("#myCarousel").carousel();
+//
+//   // Enable Carousel Indicators
+//   $(".item").click(function(){
+//     $("#myCarousel").carousel(1);
+//   });
+//
+//   // Enable Carousel Controls
+//   $(".left").click(function(){
+//     $("#myCarousel").carousel("prev");
+//   });
+// });
--- project_backend/public/scripts/index.js
+++ project_backend/public/scripts/index.js
@@ -1,12 +1,15 @@
-// Activate Carousel
-$("#myCarousel").carousel();
-
-// Enable Carousel Indicators
-$(".item").click(function(){
-  $("#myCarousel").carousel(1);
-});
-
-// Enable Carousel Controls
-$(".left").click(function(){
-  $("#myCarousel").carousel("prev");
-});
+// $(document).ready( {
+//
+//   // Activate Carousel
+//   $("#myCarousel").carousel();
+//
+//   // Enable Carousel Indicators
+//   $(".item").click(function(){
+//     $("#myCarousel").carousel(1);
+//   });
+//
+//   // Enable Carousel Controls
+//   $(".left").click(function(){
+//     $("#myCarousel").carousel("prev");
+//   });
+// });
